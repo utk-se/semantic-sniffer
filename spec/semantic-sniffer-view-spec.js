8c943eec5de8bf377da3460521fdf14962d4f6af
Aiden Rutter <arutter@utk.edu>
2020-11-29 16:03:16 -0500

    template package

---
+++ spec/semantic-sniffer-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SemanticSnifferView from '../lib/semantic-sniffer-view';
+
+describe('SemanticSnifferView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
