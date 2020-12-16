bda0f10a6bada73f4527c93e8d6c37a412079657
Aiden Rutter <arutter@utk.edu>
2020-12-16 01:03:31 -0500

    Add sample file for case b

---
+++ sample-files/sample_b.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    // used as sample input for testing parser vs following scenario
+    // Use of == instead of .equals to compare strings.
+    public static void main(String[] args)
+    {
+        String s = "hello";
+
+        if (s = "hello")
+        {
+            System.out.println("The above conditional should be identified as a code smell.");
+        }
+
+        if (s.equals("hello"))
+        {
+        }
+    }
+}
